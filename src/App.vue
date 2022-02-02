e9ee0a34183e56fb25c9a7a0013c230e58e35a11
eeny <59357089+eeny@users.noreply.github.com>
2022-02-03 02:01:42 +0900

    이미지 첨부, v-if, 모달창

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
+  <div class="black-bg" v-if="modalStatus == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modalStatus = false">닫기</button>
+    </div>
+  </div>
+
+
+
   <div class="menu">
     <a v-for="a in menus" :key="a">{{ a }}</a>
   </div>
 
   <div>
-    <h4>{{ products[0] }} 원룸</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalStatus = true">{{ products[0] }} 원룸</h4>
     <p>50 만원</p>
     <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{ reports[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{ products[1] }} 원룸</h4>
     <p>70 만원</p>
     <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{ reports[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{ products[2] }} 원룸</h4>
     <p>90 만원</p>
     <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{ reports[2] }}</span>
@@ -28,6 +41,7 @@ export default {
   name: 'App',
   data(){
     return {
+      modalStatus : true,
       reports : [0, 0, 0],
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -52,6 +66,29 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -62,6 +99,13 @@ export default {
   color: white;
   padding: 10px;
 }
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+
 
 
 </style>
