4e1de823a3aae6f7bbd0c8b7249b4467c2d1fb92
eeny <59357089+eeny@users.noreply.github.com>
2022-02-05 01:03:15 +0900

    v-model

--- src/Modal.vue
+++ src/Modal.vue
@@ -4,7 +4,9 @@
       <img :src="onerooms[clickNum].image" alt="" />
       <h4>{{ onerooms[clickNum].title }}</h4>
       <p>{{ onerooms[clickNum].content }}</p>
-      <p>{{ onerooms[clickNum].price }}원</p>
+      <!-- <input type="text" @input="month = $event.target.value" /> -->
+      <input type="text" v-model.number="month" />
+      <p>{{ month }}개월 선택함 : {{ onerooms[clickNum].price * month }}원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -13,9 +15,15 @@
 <script>
 export default {
   name: "Modal",
+  data() {
+    return {
+      month: 1,
+    };
+  },
   props: {
     onerooms: Array,
     clickNum: Number,
+    modalStatus: Boolean,
   },
 };
 </script>
